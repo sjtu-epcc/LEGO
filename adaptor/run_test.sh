88474e2d2b692b83ae0422c94464a3a11449efdd
Deepolar <aunnar@163.com>
2025-12-09 06:23:57 +0800

    trust remote code in lm-eval

--- adaptor/run_test.sh
+++ adaptor/run_test.sh
@@ -7,28 +7,25 @@ pip install "lm_eval[hf]"
 
 # test
 # If test original models, do not use --trust_remote_code.
-# IF test custom model, use --trust_remote_code.
+# IF test custom model, use trust_remote_code.
 
 # mmlu
 lm_eval --model hf \
-    --model_args pretrained=/state/partition/model/kehanlu_llama-3.2-8B-Instruct \
+    --model_args pretrained=/state/partition/model/kehanlu_llama-3.2-8B-Instruct,trust_remote_code=False \
     --tasks mmlu \
     --device cuda:0 \
-    --batch_size 8 \
-    --trust_remote_code
+    --batch_size 8
 
 # squadv2
 lm_eval --model hf \
-    --model_args pretrained=/state/partition/model/kehanlu_llama-3.2-8B-Instruct \
+    --model_args pretrained=/state/partition/model/kehanlu_llama-3.2-8B-Instruct,trust_remote_code=False \
     --tasks squadv2 \
     --device cuda:0 \
-    --batch_size 8 \
-    --trust_remote_code
+    --batch_size 8
 
 # arc_challenge_chat
 lm_eval --model hf \
-    --model_args pretrained=/state/partition/model/kehanlu_llama-3.2-8B-Instruct \
+    --model_args pretrained=/state/partition/model/kehanlu_llama-3.2-8B-Instruct,trust_remote_code=False \
     --tasks arc_challenge_chat \
     --device cuda:0 \
-    --batch_size 8 \
-    --trust_remote_code
+    --batch_size 8
